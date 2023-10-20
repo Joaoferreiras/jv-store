42415bec7d8a32813125b1956362a8288aff437e
Joaoferreiras <joaovictordesousaferreira1@gmail.com>
2023-10-20 12:53:14 -0300

    feat: adição da seção de teclados na home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,40 +2,51 @@ import Image from "next/image";
 import Categories from "./components/categories";
 import { prismaClient } from "@/lib/prisma";
 import ProductList from "./components/productList";
+import SectionTitle from "./components/sectionTitle";
+import PromoBanner from "./components/promoBanner";
 
 export default async function Home() {
   const deals = await prismaClient.product.findMany({
+    where: {
+      discountPercentage: {
+        gt: 0,
+      },
+    },
+  });
+
+  const keyboards = await prismaClient.product.findMany({
     where:{
-      discountPercentage:{
-        gt:0,
+      category:{
+        slug:"keyboards"
       }
     }
   })
   return (
     <div>
-      <Image
-       src="/BannerHome01.png"
-       height={0}
-       width={0}
-       className="h-auto w-full px-5"
-       sizes="100vw"
-       alt="Até 55% de desconto esse mês!" />
+      <PromoBanner
+        src="/BannerHome01.png"
+        alt="Até 55% de desconto esse mês!"
+      />
 
-       <div className="mt-8 px-5">
-        <Categories/>
-       </div>
+      <div className="mt-8 px-5">
+        <Categories />
+      </div>
 
-       <div className="mt-8">
-        <p className="font-bold uppercase pl-5 mb-3">Ofertas</p>
+      <div className="mt-8">
+      <SectionTitle>Ofertas</SectionTitle>
         <ProductList products={deals} />
-       </div>
-       <Image
-       src="/BannerHome02.png"
-       height={0}
-       width={0}
-       className="h-auto w-full px-5"
-       sizes="100vw"
-       alt="Até 55% de desconto em mouses!" />
+      </div>
+
+      <PromoBanner
+        src="/BannerHome02.png"
+        alt="Até 55% de desconto em mouses!"
+      />
+
+<div className="mt-8">
+        <SectionTitle>Ofertas</SectionTitle>
+        <ProductList products={keyboards} />
+      </div>
+
     </div>
   );
 }
