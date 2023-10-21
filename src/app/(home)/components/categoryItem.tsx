487da5f9035597ec798972b26e5635f9e287560f
Joaoferreiras <joaovictordesousaferreira1@gmail.com>
2023-10-20 21:02:17 -0300

    feat: add de página de produtos de uma categoria

--- src/app/(home)/components/categoryItem.tsx
+++ src/app/(home)/components/categoryItem.tsx
@@ -1,25 +1,19 @@
 import { Badge } from "@/components/ui/badge";
+import { CATEGORY_ICON } from "@/constants/categoryIcons";
 import { Category } from "@prisma/client";
-import { HeadphonesIcon, KeyboardIcon, MonitorIcon, MouseIcon, SpeakerIcon, SquareIcon } from "lucide-react";
-
-interface CategoryItemProps{
-    category: Category
+interface CategoryItemProps {
+  category: Category;
 }
 
-const CategoryItem = ({category}: CategoryItemProps) => {
-    const categoryIcon = {
-        keyboards: <KeyboardIcon size={16}/>,
-        monitors: <MonitorIcon size={16}/>,
-        headphones: <HeadphonesIcon size={16}/>,
-        mousepads: <SquareIcon size={16}/>,
-        speakers: <SpeakerIcon size={16}/>,
-        mouses: <MouseIcon size={16}/>
-    }
-    return ( 
-        <Badge variant="outline" className="flex items-center justify-center py-3 gap-2 rounded-lg">
-            {categoryIcon[category.slug as keyof typeof categoryIcon]}
-            <span className="text-xs font-bold">{category.name}</span>
-        </Badge>
-     );
-}
- export default CategoryItem;
+const CategoryItem = ({ category }: CategoryItemProps) => {
+  return (
+    <Badge
+      variant="outline"
+      className="flex items-center justify-center py-3 gap-2 rounded-lg"
+    >
+      {CATEGORY_ICON[category.slug as keyof typeof CATEGORY_ICON]}
+      <span className="text-xs font-bold">{category.name}</span>
+    </Badge>
+  );
+};
+export default CategoryItem;
