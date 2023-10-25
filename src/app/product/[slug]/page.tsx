81edcb52ad1f847fdb3746cce1a10211f59ed2d9
Joaoferreiras <joaovictordesousaferreira1@gmail.com>
2023-10-25 19:50:28 -0300

    feat: exibição de produtos recomendados na tela de produtos

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -2,6 +2,7 @@ import { prismaClient } from "@/lib/prisma";
 import ProductImage from "./components/productImages";
 import ProductInfo from "./components/productInfo";
 import { computeProductTotalPrice } from "@/helpers/product";
+import ProductList from "@/components/ui/productList";
 
 interface ProductDetailsPageProps{
     params:{
@@ -13,13 +14,26 @@ const ProductDetailsPage = async ({params: {slug}}: ProductDetailsPageProps) => 
     const product = await prismaClient.product.findFirst({
         where:{
             slug: slug
+        }, include:{
+            category:{
+                include:{
+                    products:{
+                        where:{
+                            slug:{
+                                not: slug
+                            }
+                        }
+                    }
+                }
+            }
         }
     });
 
     if(!product) return null
-    return <div className="flex flex-col gap-8">
+    return <div className="flex flex-col gap-8 pb-8">
         <ProductImage imageUrls={product.imageUrls} name={product.name}/>
         <ProductInfo product={computeProductTotalPrice(product)}/>
+        <ProductList products={product.category.products}/>
     </div>
 }
  
