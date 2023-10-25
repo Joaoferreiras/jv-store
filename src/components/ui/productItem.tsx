d6f471179605b67a2eec48e7995e50451d35b0ee
Joaoferreiras <joaovictordesousaferreira1@gmail.com>
2023-10-25 15:31:18 -0300

    feat: rota para os detalhes do produto ao clicar no produto

--- src/components/ui/productItem.tsx
+++ src/components/ui/productItem.tsx
@@ -2,6 +2,7 @@ import { ProductWithTotalPrice } from "@/helpers/product";
 import { ArrowDownIcon } from "lucide-react";
 import { Badge } from "./badge";
 import Image from "next/image";
+import Link from "next/link";
 
 interface ProductItemProps {
   product: ProductWithTotalPrice;
@@ -9,6 +10,7 @@ interface ProductItemProps {
 
 const ProductItem = ({ product }: ProductItemProps) => {
   return (
+   <Link href={`/product/${product.slug}`}>
     <div className="flex flex-col gap-4">
       <div className="relative flex h-[170px] w-full items-center justify-center rounded-lg bg-accent">
         <Image
@@ -53,6 +55,7 @@ const ProductItem = ({ product }: ProductItemProps) => {
         </div>
       </div>
     </div>
+   </Link>
   );
 };
 
