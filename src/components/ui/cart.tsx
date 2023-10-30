7db3b179f0be9e5f4a3ba6c797d6cc2df5f4b36d
Joaoferreiras <joaovictordesousaferreira1@gmail.com>
2023-10-30 14:05:18 -0300

    feat: adição do Stripe checkout no carrinho

--- src/components/ui/cart.tsx
+++ src/components/ui/cart.tsx
@@ -7,9 +7,24 @@ import { computeProductTotalPrice } from "@/helpers/product";
 import { Separator } from "@radix-ui/react-separator";
 import { ScrollArea } from "./scroll-area";
 import { Button } from "./button";
+import { createCheckout } from "@/actions/checkout";
+import { loadStripe } from "@stripe/stripe-js";
+
 
 const Cart = () => {
   const { products, subTotal, total, totalDiscount } = useContext(cartContext);
+
+  const handleFinishPurchaseClick = async ()=>{
+   const checkout = await createCheckout(products)
+
+   const stripe = await loadStripe(
+    process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY
+  )
+    
+stripe?.redirectToCheckout({
+  sessionId: checkout.id
+})
+  };
 
   return (
     <div className="flex flex-col gap-8 h-full">
@@ -67,7 +82,7 @@ const Cart = () => {
           <p>R$ {total.toFixed(2)}</p>
         </div>
 
-        <Button className="uppercase font-bold mt-7">Finalizar compra</Button>
+        <Button className="uppercase font-bold mt-7" onClick={handleFinishPurchaseClick}>Finalizar compra</Button>
       </div>
     </div>
   );
